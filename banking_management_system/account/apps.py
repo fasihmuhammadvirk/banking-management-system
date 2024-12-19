e82b7816cf860f9376f2a795c688925ff6a33a23
Fasih Muhammad Virk <fasih.muhammad@icloud.com>
2024-12-19 18:31:42 +0500

    change the complete architecture of the app

--- banking_management_system/account/apps.py
+++ banking_management_system/account/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class BankingConfig(AppConfig):
+class AccountConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'banking'
+    name = 'account'
--- banking_management_system/banking_management_system/settings.py
+++ banking_management_system/banking_management_system/settings.py
@@ -30,16 +30,27 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+
+PREDEF_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles',]
 
-    'banking.apps.BankingConfig'
+PROJECT_APPS = [
+    'bank.apps.BankConfig',
+    'account.apps.AccountConfig',
+    'transaction.apps.TransactionConfig',
+    'user.apps.UserConfig'
 ]
+
+INSTALLED_APPS = PREDEF_APPS + PROJECT_APPS
+
+
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -120,6 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static',
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
