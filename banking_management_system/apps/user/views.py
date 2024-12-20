2958e2792adc6090fb9d6904e1f0633ff3e82805
Fasih Muhammad Virk <fasih.muhammad@icloud.com>
2024-12-20 15:10:22 +0500

    update the html template fro bank and account

--- banking_management_system/apps/user/views.py
+++ banking_management_system/apps/user/views.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render , redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
+@login_required
 def home(request):
     user = request.user
     context = {
