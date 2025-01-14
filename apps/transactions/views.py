071677ce84a5077908cdb16676202031b1393ffa
Fasih Muhammad Virk <fasih.muhammad@icloud.com>
2025-01-14 13:13:46 +0500

    fix naming convention

--- apps/transactions/views.py
+++ apps/transactions/views.py
@@ -16,13 +16,13 @@ class TransactionListView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         account_number_from_query = self.request.GET.get('account_number')
-        user_information = Account.objects.filter(account_number=account_number_from_query).first()
-        all_user_transaction = Transaction.objects.filter(account=user_information)
+        user_account_data = Account.objects.filter(account_number=account_number_from_query).first()
+        all_user_transaction = Transaction.objects.filter(account=user_account_data)
         return all_user_transaction
 
 
 class TransactionCreateView(LoginRequiredMixin, CreateView):
-    template_name = 'transactions/create_transaction.html'
+    template_name = 'transactions/make_transaction.html'
     form_class = MakeTransactionForm
     login_url = 'login/'
     redirect_field_name = 'next'
@@ -36,25 +36,25 @@ class TransactionCreateView(LoginRequiredMixin, CreateView):
         amount_user_entered = int(request.POST.get('amount'))
         transaction_type_user_select = request.POST.get('transaction_type')
 
-        user_information = Account.objects.filter(account_number=account_number_from_query).first()
+        user_account_data = Account.objects.filter(account_number=account_number_from_query).first()
 
         Transaction.objects.create(
-            account=user_information,
+            account=user_account_data,
             amount=amount_user_entered,
             transaction_type=transaction_type_user_select,
         )
 
-        if transaction_type_user_select == "Withdrawal" and amount_user_entered <= user_information.balance:
-            user_information.balance -= amount_user_entered
-            context['messages'] = [f'Transaction Successful Current Balance: {user_information.balance}']
+        if transaction_type_user_select == "Withdrawal" and amount_user_entered <= user_account_data.balance:
+            user_account_data.balance -= amount_user_entered
+            context['messages'] = [f'Transaction Successful Current Balance: {user_account_data.balance}']
 
         elif transaction_type_user_select == "Deposit":
-            user_information.balance += amount_user_entered
-            context['messages'] = [f'Transaction Successful Current Balance: {user_information.balance}']
+            user_account_data.balance += amount_user_entered
+            context['messages'] = [f'Transaction Successful Current Balance: {user_account_data.balance}']
 
         else:
             context['messages'] = ["Please enter a correct amount"]
 
-        user_information.save()
+        user_account_data.save()
 
         return render(request, self.template_name, context)
