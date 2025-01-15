9300553c9328d2f995bc4d5c1faf3b378cc59f49
Fasih Muhammad Virk <fasih.muhammad@icloud.com>
2025-01-15 11:50:43 +0500

    fix absolute path

--- apps/transactions/models.py
+++ apps/transactions/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from ..accounts.models import Account
+from apps.accounts.models import Account
 
 
 class Transaction(models.Model):
--- apps/transactions/views.py
+++ apps/transactions/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from ..accounts.models import Account
-from .models import Transaction
+from apps.accounts.models import Account
+from apps.transactions.models import Transaction
 
 
 @login_required
