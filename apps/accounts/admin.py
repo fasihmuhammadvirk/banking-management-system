2e615ed372553b64a0c144f67c5aa27c46b38223
Fasih Muhammad Virk <fasih.muhammad@icloud.com>
2025-01-16 17:13:30 +0500

    implemented iexact on account search feild

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -7,4 +7,4 @@ from apps.accounts.models import Account
 class AccountAdmin(admin.ModelAdmin):
     list_display = ('user__username', 'bank__name', 'account_number', 'balance')
     list_filter = ('user__username', 'bank__name', 'created_at')
-    search_fields = ('user__username', 'bank__name')
+    search_fields = ('=user__username', 'bank__name')
