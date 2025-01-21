2d908db76da9bce028b3000cdd091f7d4bd12b99
Fasih Muhammad Virk <fasih.muhammad@icloud.com>
2025-01-21 13:22:27 +0500

    updated utils

--- apps/transactions/views.py
+++ apps/transactions/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-from apps.transactions.utils import get_create_transaction_context
+from apps.transactions.utils import update_user_balance_and_create_transaction_history
 from apps.transactions.models import Transaction
 from apps.accounts.models import Account
 
@@ -21,14 +21,19 @@ def transactions_list_view(request):
 
 @login_required
 def transaction_create_view(request):
+    context = dict()
     account_number_from_query = request.GET.get('account_number')
 
     if request.method == "POST" and account_number_from_query:
         amount_user_entered = int(request.POST.get('amount'))
-        transaction_type_user_select = request.POST.get('transaction_type')
+        transaction_type_user_select = str(request.POST.get('transaction_type'))
 
-        context = get_create_transaction_context(amount_user_entered, transaction_type_user_select)
+        is_transaction_successful = update_user_balance_and_create_transaction_history(transaction_type_user_select,
+                                                                                       amount_user_entered,
+                                                                                       account_number_from_query)
+        if is_transaction_successful:
+            context = ['Transaction Successful']
+        else:
+            context = ['Please try again something went wrong']
 
     return render(request, 'transactions/create_transaction.html', context)
-
-
