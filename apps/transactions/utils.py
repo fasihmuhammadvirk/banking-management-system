68c294ef30ae7cd2c7f21a5c1662c0ae4168c8dc
Fasih Muhammad Virk <fasih.muhammad@icloud.com>
2025-01-21 13:45:23 +0500

    update transaction view

--- apps/transactions/utils.py
+++ apps/transactions/utils.py
@@ -2,33 +2,27 @@ from apps.transactions.models import Transaction
 from apps.accounts.models import Account
 
 
-def get_create_transaction_context(account_number_from_query, amount_user_entered, transaction_type_user_select):
-    user_information = Account.objects.filter(account_number=account_number_from_query).first()
-
-    process_transaction_flag = False
+def update_user_balance_and_create_transaction_history(transaction_type_user_select: str,
+                                                       amount_user_entered: int, account_number: str) -> bool:
+    user_information = Account.object.filter(account_number=account_number)
+    process_success_flag = False
 
     if transaction_type_user_select == "Withdrawal" and amount_user_entered <= user_information.balance:
         user_information.balance -= amount_user_entered
-        process_transaction_flag = True
+        process_success_flag = True
 
-    if transaction_type_user_select == "Deposit":
+    elif transaction_type_user_select == "Deposit":
         user_information.balance += amount_user_entered
-        process_transaction_flag = True
-
-    if process_transaction_flag:
-
-        Transaction.objects.create(
-            account=user_information,
-            amount=amount_user_entered,
-            transaction_type=transaction_type_user_select,
-        )
-
-        user_information.save()
-        context = {"message": f"Transaction Successful Your Current Balance is {user_information.balance}"}
-
-        return context
+        process_success_flag = True
 
     else:
+        return process_success_flag
 
-        context = {"message": "Please enter the correct amount"}
-        return context
+    Transaction.objects.create(
+        account=user_information,
+        amount=amount_user_entered,
+        transaction_type=transaction_type_user_select,
+    )
+
+    user_information.save()
+    return process_success_flag
--- apps/transactions/views.py
+++ apps/transactions/views.py
@@ -1,11 +1,11 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, FormView
 from apps.transactions.forms import MakeTransactionForm
 
 from apps.accounts.models import Account
 from apps.transactions.models import Transaction
-from apps.transactions.utils import get_create_transaction_context
+from apps.transactions.utils import update_user_balance_and_create_transaction_history
 
 
 class TransactionListView(LoginRequiredMixin, ListView):
@@ -22,18 +22,27 @@ class TransactionListView(LoginRequiredMixin, ListView):
         return all_user_transaction
 
 
-class TransactionCreateView(LoginRequiredMixin, CreateView):
+class TransactionCreateView(LoginRequiredMixin, FormView):
     template_name = 'transactions/make_transaction.html'
     form_class = MakeTransactionForm
     login_url = 'login/'
     redirect_field_name = 'next'
 
-    def post(self, request):
-        account_number_from_query = request.GET.get('account_number')
+    def form_valid(self, form):
+        context = dict()
+        account_number_from_query = self.request.GET.get('account_number')
 
-        if request.method == "POST" and account_number_from_query:
-            amount_user_entered = int(request.POST.get('amount'))
-            transaction_type_user_select = request.POST.get('transaction_type')
-            context = get_create_transaction_context(amount_user_entered, transaction_type_user_select)
+        if account_number_from_query:
+            amount_user_entered = int(form.cleaned_data['amount'])
+            transaction_type_user_select = form.cleaned_data['transaction_type']
 
-        return render(request, self.template_name, context)
+            is_transaction_successful = update_user_balance_and_create_transaction_history(
+                transaction_type_user_select,
+                amount_user_entered,
+                account_number_from_query)
+
+            if is_transaction_successful:
+                context = ['Transaction Successful']
+            else:
+                context = ['Please try again something went wrong']
+        return render(form, self.template_name, context)
