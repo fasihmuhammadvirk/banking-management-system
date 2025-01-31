8d200e6de2b637cce4397d2ab22735297c552e31
Fasih Muhammad Virk <82199241+fasihmuhammadvirk@users.noreply.github.com>
2025-01-31 17:43:39 +0500

    added create user functionality

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from apps.accounts.views import UserListView
+from apps.accounts.views import UserListView, UserCreateView
 
 urlpatterns = [
 
     path("", UserListView.as_view(), name='accounts-list'),
-
+    path("create/", UserCreateView.as_view(), name='create-account')
 ]
